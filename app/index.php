6be95f707edb0f0a9570e936423eb5fdab01b227
psychopomp3 <psychopompe@protonmail.com>
2023-09-15 11:21:12 +0200

    Ajout variables à 'index' et création SyntaxBranch

--- app/index.php
+++ app/index.php
@@ -1,19 +1,13 @@
-<?php
 
-
-//commentaires
-/*commentaires sur
-plusieurs lignes*/
-//phpinfo();
-
-echo "test";
-
-?>
-
-<h1>titre H1 en html wesh</h1>
+<h1>titre H1 en html</h1>
 
 <?php
 
-
+//variables
+$variable = 'une variable';
+$VariaBle = 'une autre variable';
+echo $variable;
+echo '<br>';
+echo $VariaBle;
 
 ?>
