dc5e69092ac8da93960a0ee04f339eaf1ab0c815
psychopomp3 <psychopompe@protonmail.com>
2023-09-18 09:48:43 +0200

    Rajout "Structures Controles" (230918)

--- app/inclusion/partials/menu.php
+++ app/inclusion/partials/menu.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="">Home</a></li>
-    <li><a href="">Structures de contrôles</a></li>
+    <li><a href="../../struc-control">Structures de contrôles</a></li>
     <li><a href="">Boucles</a></li>
     <li><a href="">Tableaux</a></li>
     <li><a href="">Fonctions</a></li>
