4bc7858e424bf385261c92fba8cb8b74b8a9a227
psychopomp3 <psychopompe@protonmail.com>
2023-09-18 10:35:03 +0200

    Exercices de If

--- app/struc-control/index.php
+++ app/struc-control/index.php
@@ -1,3 +1,30 @@
 <?php
-echo "test";
+echo "test du index de 'struc-control'";
+echo '<br/><br/><br/>';
+
+/*--conditions : if, else, elseif ------------------------------------------------*/
+
+$age = 10;
+if($age >= 18) {
+    echo "Vous avez le droit de voter";
+    echo '<br/>';
+} else {
+    echo "Vous n'avez pas le droit de voter";
+    echo '<br/><br/><br/>';
+}
+
+$heure = 23;
+/* afficher 'on est le matin' lorsque l'heure est inférieure à 12h
+ et 'on est le soir' lorsque l'heure est supérieure à 18h*/
+if($heure < 12) {
+    echo "On est le matin";
+    echo '<br/>';
+} elseif($heure >= 18) {
+    echo "On est le soir";
+    echo '<br/>';
+} else {
+    echo "On est l'après-midi";
+    echo '<br/><br/><br/>';
+}
+
 ?>
