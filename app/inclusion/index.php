5f00252bf80a664674a44d913d1bf0fe2aca59dc
psychopomp3 <psychopompe@protonmail.com>
2023-09-18 09:33:45 +0200

    230916 recup files

---
+++ app/inclusion/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //include
+        // include('./partials/header.php');
+        // include('./partials/footer.php');
+        //require
+        // require('./partials/header.php');
+            // require_once('./partials/header.php');
+        // require('./partials/footer.php');
+    ?>
+<?php include('./partials/header.php'); ?>
+<main>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum possimus non vel fugit voluptatum sed debitis voluptates nesciunt, laboriosam dicta qui eos nisi, placeat incidunt blanditiis totam. Doloremque, aut!</p>
+        <p>Quam error excepturi similique enim aspernatur illo atque maxime laborum minus, sed doloremque quaerat asperiores accusamus neque dolores veritatis quo nihil quisquam. Tempore ab nobis quasi voluptates, iste hic fugit!</p>
+        <p>Voluptatibus possimus accusamus officiis esse a hic excepturi minus doloremque incidunt maxime in architecto corrupti maiores libero ut nihil soluta eveniet repudiandae tenetur, enim recusandae, provident facilis nulla officia. Aliquid?</p>
+    </section>
+</main>
+<?php include('./partials/footer.php'); ?>
+</body>
+</html>
---
+++ app/inclusion/partials/menu.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Structures de contrôles</a></li>
+    <li><a href="">Boucles</a></li>
+    <li><a href="">Tableaux</a></li>
+    <li><a href="">Fonctions</a></li>
+</ul>
