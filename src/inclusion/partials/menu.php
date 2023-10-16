437db3a9362c1f9e5ef2dee4b4b7c3f3e53abdaa
psychopomp3 <psychopompe@protonmail.com>
2023-10-16 13:29:23 +0200

    Reprise de l'arborescence ajout d'un rep "Syntaxe"

--- src/inclusion/partials/menu.php
+++ src/inclusion/partials/menu.php
@@ -1,9 +1,9 @@
 <ul>
-    <li><a href="">Home</a></li>
-    <li><a href="../../1-struc-control/strucon.php">Structures de contrôles</a></li>
-    <li><a href="../../2-tableaux/tab.php">Tableaux</a></li>
-    <li><a href="../../3-fonctions/fonc.php">Fonctions</a></li>
-    <li><a href="../../4-boucles/bouc.php">Boucles</a></li>
-    <li><a href="../../5-boucles-tableaux/bouctab.php">Boucles-Tableaux</a></li>
-    <li><a href="../../6-formulaires/form.php">Formulaires</a></li>
+    <li><a href="../../1-syntaxe/syntax.php">Syntaxe de base</a></li>
+    <li><a href="../../2-struc-control/strucon.php">Structures de contrôles</a></li>
+    <li><a href="../../3-tableaux/tab.php">Tableaux</a></li>
+    <li><a href="../../4-fonctions/fonc.php">Fonctions</a></li>
+    <li><a href="../../5-boucles/bouc.php">Boucles</a></li>
+    <li><a href="../../6-boucles-tableaux/bouctab.php">Boucles-Tableaux</a></li>
+    <li><a href="../../7-formulaires/form.php">Formulaires</a></li>
 </ul>
